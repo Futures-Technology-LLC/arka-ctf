45725334e6870e3f9bcf5a051a0cbfaf9c38f410
Jinank Jain <jinank@futurestech.xyz>
2025-02-13 15:30:24 +0530

    Make usdc account optional
    Signed-off-by: Jinank Jain <jinank@futurestech.xyz>

--- programs/solana-ctf/tests/mint.rs
+++ programs/solana-ctf/tests/mint.rs
@@ -404,7 +404,7 @@ async fn transfer_from_user_wallet_to_pda(
         usdc_mint: usdc_mint.mint.pubkey(),
         escrow_account: escrow_pda,
         delegate: delegate_account,
-        user_usdc_token_account: user.user_usdc_ata.clone(),
+        user_usdc_token_account: Some(user.user_usdc_ata.clone()),
         promo_account: Some(promo_pda),
         promo_delegate: Some(promo_pda),
     };
